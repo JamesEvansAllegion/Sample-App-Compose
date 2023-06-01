4cab385baac7c45f7616adcc4239ba5e8caf2550
James Evans <james.evans@allegion.com>
2023-06-01 11:53:27 -0500

    Test Commit

--- app/src/main/java/com/example/sampleappcompose/MainActivity.kt
+++ app/src/main/java/com/example/sampleappcompose/MainActivity.kt
@@ -15,7 +15,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateListOf
