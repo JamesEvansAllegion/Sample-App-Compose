bb45a1a3677c95c51241f361a8e924d16e75bcb3
James Evans <james.evans@allegion.com>
2023-06-02 08:44:41 -0500

    Added Home Screen, App switches between states

--- app/src/main/java/com/example/sampleappcompose/MainActivity.kt
+++ app/src/main/java/com/example/sampleappcompose/MainActivity.kt
@@ -23,6 +23,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -36,6 +37,12 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -48,7 +55,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SearchApp()
+            MainScreen()
         }
     }
 }
@@ -70,7 +77,7 @@ fun TopBarPreview() {
 
 
 @Composable
-fun BottomNavigationBar() {
+fun BottomNavigationBar(navController: NavController) {
     val items = listOf(
         Screens.Home,
         Screens.Search,
@@ -78,8 +85,10 @@ fun BottomNavigationBar() {
     )
     BottomNavigation(
         backgroundColor = Color.White,
-        contentColor = Color.White
+        contentColor = Color.Black
     ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
         items.forEach { item ->
             BottomNavigationItem(
                 icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
@@ -87,39 +96,65 @@ fun BottomNavigationBar() {
                 selectedContentColor = Color.White,
                 unselectedContentColor = Color.White.copy(0.4f),
                 alwaysShowLabel = true,
-                selected = false,
+                selected = currentRoute == item.route,
                 onClick = {
-                    /* TODO */
+                    navController.navigate(item.route) {
+                        navController.graph.startDestinationRoute?.let { route ->
+                            popUpTo(route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
                 }
             )
         }
     }
 }
-@Preview(showBackground = true)
-@Composable
-fun BottomNavigationBarPreview() {
-    BottomNavigationBar()
-}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun BottomNavigationBarPreview() {
+//    BottomNavigationBar()
+//}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val navController = rememberNavController()
     Scaffold(
         topBar = { TopBar() },
-        bottomBar = { BottomNavigationBar() },
-        content = { padding -> Box(modifier = Modifier.padding(padding)) {
-                /* TODO */
+        bottomBar = { BottomNavigationBar(navController) },
+        content = { padding -> // We have to pass the scaffold inner padding to our content. That's why we use Box.
+            Box(modifier = Modifier.padding(padding)) {
+                Navigation(navController = navController)
             }
         }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun MainScreenPreview() {
-    MainScreen()
-}
+//@Preview(showBackground = true)
+//@Composable
+//fun MainScreenPreview() {
+//    MainScreen()
+//}
 
+
+@Composable
+fun Navigation(navController: NavHostController) {
+    NavHost(navController, startDestination = Screens.Home.route) {
+        composable(Screens.Home.route) {
+            HomeScreen()
+        }
+        composable(Screens.Search.route) {
+            SearchScreen()
+        }
+        composable(Screens.Info.route) {
+            InfoScreen()
+        }
+    }
+}
 
 
 
